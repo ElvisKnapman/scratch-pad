5f75f6aa3211bcb55a4c7da4dbfe1def620688e3
Elvis Knapman <elvis@elvisknapman.com>
2019-02-27 22:26:28 -0500

    Practice getting time stamps in milliseconds and comparing them

--- dates/dates.js
+++ dates/dates.js
@@ -18,3 +18,22 @@ console.log(months[currentMonth]);
 
 const currentDay = now.getDay();
 console.log(days[currentDay]);
+
+
+// Unix Epoch - January 1st 1970 00:00
+
+const dateOne = new Date('May 10 2001 12:00:00');
+const dateTwo = new Date('May 10 2003 12:00:00');
+
+// get time of dates in milliseconds (calculate time elapsed from January 1st 1970)
+const dateOneTimestamp = dateOne.getTime();
+const dateTwoTimestamp = dateTwo.getTime();
+
+// check which date came first by comparing their 'timestamp' which is in milliseconds
+if (dateOneTimestamp < dateTwoTimestamp) {
+    console.log(`Date 1 came first: ${dateOne.toString()}`);
+} else if (dateTwoTimestamp < dateOneTimestamp) {
+    console.log(`Date 2 came first: ${dateTwo.toString()}`);
+} else {
+    console.log(`The dates are the same.`);
+}
