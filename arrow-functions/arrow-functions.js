1492d1f026942af8ccb3257fa672d1cbda1f81b3
Elvis Knapman <elvis@elvisknapman.com>
2019-03-06 19:24:07 -0500

    Initial commit

---
+++ arrow-functions/arrow-functions.js
@@ -0,0 +1,7 @@
+// arrow function
+const square = (num) => {
+    return num * num;
+}
+
+// one line return - can omit brackets if just one parameter and omit curly braces and return keyword if it's just a return statement in the function body
+const square = num => num * num;
